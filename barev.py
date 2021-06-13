84fe7799c8dc201ec011ed959d5f5649a4db394b
Souren Papazian <souren@papazian.info>
2021-06-13 09:24:49 -0700

    Popokhakan avelatsretsi

--- barev.py
+++ barev.py
@@ -1,2 +1,6 @@
-for i in range(5):
-    print("Barev tikin myakhkiznak, duk tsakhiki chap sirun ek!")
+usuchich = "tikin Tvoydiznak"
+qanak = 5
+inch = "gata"
+adzakan = "hianali"
+for i in range(qanak):
+    print("Barev " + usuchich + " duk " + inch + " chap " + adzakan + " ek!")
